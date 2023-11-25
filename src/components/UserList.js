0cde302b8ef3f9b6e775f3c234dacf1c64ccc7da
Himanshusharma83 <hs9450331@gmail.com>
2023-11-25 10:41:18 +0530

    Coding studion changes

---
+++ src/components/UserList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Spin, Row, Col } from 'antd';
+import UserProfile from './UserProfile';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleDelete = (userId) => {
+    // Filter out the deleted user
+    const updatedUsers = users.filter((user) => user.id !== userId);
+    setUsers(updatedUsers);
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: 'calc(100vh - 40px)', // Adjust as needed
+          }}
+        >
+          <Spin size="large" />
+        </div>
+      ) : (
+        <Row gutter={[16, 16]}>
+          {users.map((user) => (
+            <Col key={user.id} xs={24} sm={12} md={8} lg={6}>
+              <UserProfile user={user} onDelete={handleDelete} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </div>
+  );
+};
+
+export default UserList;
---
+++ src/components/UserProfile.js
@@ -0,0 +1,158 @@
+import React, { useState } from 'react';
+import { Card, Avatar, notification, Modal, Form, Input, message } from 'antd';
+import {
+  MailOutlined,
+  PhoneOutlined,
+  GlobalOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  HeartOutlined,
+  HeartFilled,
+} from '@ant-design/icons';
+import './UserProfile.css';
+
+
+const UserProfile = ({ user,onDelete }) => {
+  const [liked, setLiked] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editedUser, setEditedUser] = useState(user);
+
+  const handleLike = () => {
+    setLiked(!liked);
+    const message = liked ? 'Unliked!' : 'Liked';
+    const color = liked ? '#ff4d4f' : '#fff';
+    notification.success({
+      message,
+      description: `You ${message.toLowerCase()} ${editedUser.name}'s Profile`,
+      style: {
+        backgroundColor: color,
+      },
+    });
+  };
+
+  const handleEditClick = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleSave = () => {
+    console.log('Edited User:', editedUser);
+    
+    notification.success({
+      message: 'Updated!',
+      description: `${user.name}'s Profile has been updated sucessfully`
+    })
+    
+
+    setIsModalVisible(false);
+  };
+
+  const handleDelete = () => {
+    
+    onDelete(user.id); 
+    notification.success({
+      message: 'Deleted!',
+      description: `${editedUser.name}'s Profile has been deleted.`,
+      style: {
+        backgroundColor: '#ff4d4f',
+      },
+    });
+  };
+
+
+  return (
+    <Card
+      bordered={true}
+      style={{ width: '100%', marginBottom: 20 }}
+      actions={[
+        <span onClick={handleLike}>
+          {liked ? (
+            <HeartFilled style={{ fontSize: '20px', color: '#ff4d4f' }} />
+          ) : (
+            <HeartOutlined style={{ fontSize: '20px', color: '#ff4d4f' }} />
+          )}
+        </span>,
+        <EditOutlined key="edit" style={{ fontSize: '20px' }} onClick={handleEditClick} />,
+        <DeleteOutlined key="delete" style={{ fontSize: '20px' }} onClick={handleDelete} />,
+      ]}
+    >
+      <div className="custom-image" >
+        <Avatar
+          style={{ marginTop: '-2.5%' }}
+          className="avatar-full-size"
+          size={140}
+          src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${editedUser.name}`}
+        />
+      </div>
+      <div className="custom-card">
+        <h4 className='name' >
+          {editedUser.name}
+        </h4>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          <li>
+            <Card.Meta
+              style={{ marginBottom: '8px' }}
+              avatar={<MailOutlined />}
+              description={<span className='email' >{editedUser.email}</span>}
+            />
+          </li>
+          <li>
+            <Card.Meta
+              style={{ marginBottom: '8px' }}
+              avatar={<PhoneOutlined className='phone' />}
+              description={<span className='number' >{editedUser.address.zipcode}</span>}
+            />
+          </li>
+          <li>
+            <Card.Meta
+              style={{ marginBottom: '8px' }}
+              avatar={<GlobalOutlined />}
+              description={<span className='website' >{editedUser.website}</span>}
+            />
+          </li>
+        </ul>
+      </div>
+
+      <Modal
+        title="Edit Profile"
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        onOk={handleSave}
+        okText="Save"
+        cancelText="Cancel"
+      >
+        <Form initialValues={editedUser}>
+          <Form.Item label="Name" name="name">
+            <Input
+              value={editedUser.name}
+              onChange={(e) => setEditedUser({ ...editedUser, name: e.target.value })}
+            />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input
+              value={editedUser.email}
+              onChange={(e) => setEditedUser({ ...editedUser, email: e.target.value })}
+            />
+          </Form.Item>
+          <Form.Item label="Phone" name="phone">
+            <Input
+              value={editedUser.phone}
+              onChange={(e) => setEditedUser({ ...editedUser, phone: e.target.value })}
+            />
+          </Form.Item>
+          <Form.Item label="Website" name="website">
+            <Input
+              value={editedUser.website}
+              onChange={(e) => setEditedUser({ ...editedUser, website: e.target.value })}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Card>
+  );
+};
+
+export default UserProfile;
